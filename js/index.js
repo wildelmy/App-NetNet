a7ad0e982c085ad6448f44876e40ad4d03ba4786
wildelmy <84979761+wildelmy@users.noreply.github.com>
2021-06-17 23:53:53 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,49 @@
-const inputSearch = document.getElementById('input-search')
-const btnSearch = document.getElementById('btn-search')
-const initialSearch = "Batman"
+const inputSearch = document.getElementById("input-search");
+const btnSearch = document.getElementById("btn-search");
+const initialSearch = "Batman";
 
-btnSearch.addEventListener('click', getMoviesBySearch) 
+btnSearch.addEventListener("click", getMoviesBySearch);
 
+getMovies();
 async function getMovies() {
-  let response = await fetch(`https://www.omdbapi.com/?s=${initialSearch}&page=1&apikey=2499b840`);
+  // petición para obtener peliculas
+  let response = await fetch(
+    `https://www.omdbapi.com/?s=${initialSearch}&page=1&apikey=2499b840`
+  );
+  // convierte datos en json
   let data = await response.json();
   fillMovies(data);
 }
-getMovies();
-
 
 async function getMoviesBySearch() {
-  let searchValue = inputSearch.value.trim()
-  let response = await fetch(`https://www.omdbapi.com/?s=${searchValue}&page=1&apikey=2499b840`);
+  // obtiene valor busqueda
+  let searchValue = inputSearch.value.trim();
+  // petición para obtener peliculas de busqueda
+  let response = await fetch(
+    `https://www.omdbapi.com/?s=${searchValue}&page=1&apikey=2499b840`
+  );
+  // convierte datos en json
   let data = await response.json();
   fillMovies(data);
 }
 
 function fillMovies(data) {
-  let movies = document.getElementById("movies");
-  movies.innerHTML = "";
-  data.Search.forEach((movie) => {
-    movies.innerHTML += `
-    <a href=movieInfo.html?movie?=${movie.imdbID}>
+  //obtiene elemento contenedor de peliculas
+  let moviesContainer = document.getElementById("moviesContainer");
+  let movies = data.Search; //array de peliculas
+  for (let i = 0; i < movies.length; i++) {
+    //agrega un link por cada pelicula
+    // con el id de la pelicula para obtenerlo en movieInfo.html
+    moviesContainer.innerHTML += `
+    <a href=movieInfo.html?${movies[i].imdbID}>
       <div class="card">
-        <img src=${movie.Poster} class="card-img-top" height="200">
+        <img src=${movies[i].Poster} class="card-img-top" height="200">
         <div class="card-body">
-          <h5 class="card-title">${movie.Title}</h5>
-          <span>${movie.Year}</span>
+          <h5 class="card-title">${movies[i].Title}</h5>
+          <span>${movies[i].Year}</span>
         </div>
       </div>
     </a>
     `;
-  });
+  }
 }
