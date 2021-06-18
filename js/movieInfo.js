f4e90b6bcce938b8ea7289e813a3f9e9623b6d2b
wildelmy <84979761+wildelmy@users.noreply.github.com>
2021-06-17 23:54:13 -0500

    Update movieInfo.js

--- js/movieInfo.js
+++ js/movieInfo.js
@@ -2,67 +2,82 @@ const myModal = document.getElementById("modal");
 const userNameInput = document.getElementById("user");
 const btnSaveUser = document.getElementById("btn-save-user");
 
+//objeto json donde se almacenará la info de la pelicula
 let movie = {};
 
-btnSaveUser.addEventListener('click', saveUser)
+btnSaveUser.addEventListener("click", saveUser);
 
+getMovieInfo();
 async function getMovieInfo() {
-  let url = window.location;
-  let movieId = url.search.split("=")[1];
+  //obtiene el id de la pelicula de la url
+  let movieId = window.location.search.split("?")[1];
+  // petición
   let response = await fetch(
-    `https://www.omdbapi.com/?i=${movieId}&apikey=2499b840`
-  );
+    `https://www.omdbapi.com/?i=${movieId}&apikey=2499b840`);
   let data = await response.json();
+  // guarda pelicula en el objeto global
   movie = data;
   fillMovie();
 }
-getMovieInfo();
 
 function fillMovie() {
   console.log(movie);
+  // pone imagen de la pelicula
   let img = document.getElementById("poster");
   img.src = movie.Poster;
-
+  
+  // titulo de la pelicula
   let title = document.getElementById("title");
   title.innerText = movie.Title;
-
+  
+  // fecha de la pelicula
   let released = document.getElementById("released");
   released.innerText = movie.Released;
-
+  
+  // duration de la pelicula
   let duration = document.getElementById("duration");
   duration.innerText = movie.Runtime;
-
+  
+  // trama de la pelicula
   let plot = document.getElementById("plot");
   plot.innerText = movie.Plot;
 }
 
-function validateUser() {
-  let user = window.localStorage.getItem("user");
-  if (user) {
-    user = JSON.parse(user);
-    user.movies.push(movie);
-    window.localStorage.setItem("user", JSON.stringify(user));
-    window.location.href = "./shop.html";
-  } else {
-    myModal.style.display = "block";
-  }
-}
-
 function saveUser() {
+  //obtiene nombre de usuario
   let userName = userNameInput.value.trim();
+  /*
+    si hay usuario
+    crea el objeto, guarda la primera pelicula
+  */
   if (user) {
+    // guarda usuario
     let user = {
       user: userName,
       movies: [movie],
     };
+    /* convierte el objeto en string
+     guarda en localStorage
+    */
     window.localStorage.setItem("user", JSON.stringify(user));
+
+    // envía a la página de tienda
     window.location.href = "./shop.html";
   }
 }
 
 function openModal() {
-  validateUser();
+  // obtiene usuario de localStorage
+  let user = window.localStorage.getItem("user");
+  if (user) {
+    user = JSON.parse(user); // convierte en json
+    user.movies.push(movie); // agrega la pelicula seleccionada
+    window.localStorage.setItem("user", JSON.stringify(user)); // vuelve a guarda en localStorage
+    window.location.href = "./shop.html"; //envia a la tienda
+  } else {
+    myModal.style.display = "block"; //abre modal inscription
+  }
 }
 function closeModal() {
-  myModal.style.display = "none";
+  myModal.style.display = "none"; // cierra modal
 }
